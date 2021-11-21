1c8b7e6aa27be922412e29720d9bc7df47728eae
David Abanades <duckydave.abanades@gmail.com>
2021-11-21 12:02:50 +0100

    Nivell 1 - Exercici 4

---
+++ Nivell 1/Exercici4.php
@@ -0,0 +1,28 @@
+<?php
+$X = 15;
+$Y = 6;
+$N = 12.68;
+$M = 5.4;
+
+echo ("El valor de X es " . $X . "\n");
+echo ("El valor de Y es " . $Y . "\n");
+echo ("La suma de X e Y es " . ($X + $Y) . "\n");
+echo ("La resta de X e Y es " . ($X - $Y) . "\n");
+echo ("El producte de X per Y es " . ($X * $Y) . "\n");
+echo ("El modul (la resta de la divisio entre X i Y) es " . ($X % $Y) . "\n");
+echo ("\n");
+echo ("El valor de N es " . $N . "\n");
+echo ("El valor de M es " . $M . "\n");
+echo ("La suma de N e M es " . ($N + $M) . "\n");
+echo ("La resta de N e M es " . ($N - $M) . "\n");
+echo ("El producte de N per M es " . ($N * $M) . "\n");
+echo ("El modul (la resta de la divisio entre N e M) es " . ($N % $M) . "\n");
+echo ("\n");
+echo ("el doble de X es " . ($X * 2) . "\n");
+echo ("el doble de Y es " .($Y * 2) .  "\n");
+echo ("el doble de N es " .($N * 2) .  "\n");
+echo ("el doble de M es " . ($M * 2) .  "\n");
+echo ("\n");
+echo ("La suma de X, Y, N y M es " . ($X + $Y + $N + $M) . "\n");
+echo ("El producto de X por Y por N por M es " . ($X * $Y * $N * $M) . "\n");
+?>
